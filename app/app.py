65a5ce69739e8611b34071ba2047f92447a98359
Jack Langer <62244804+JackLanger@users.noreply.github.com>
2023-09-23 21:28:09 +0200

    User Views und Templates (#32)
    * chore: added new view template for index page
    
    * chore: user templating endpoint
    
    * init
    
    * chore:init
    
    * merge remote
    
    * initial
    
    * chore: merge with remote
    
    * refactor: move endpoints to controller
    
    * chore: remove redundant class

--- app/app.py
+++ app/app.py
@@ -1,24 +1,5 @@
-from flask import Flask, request
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def hello_world():  # put application's code here
-    return "Hello World!"
-
-
-@app.route("/profiles")
-def profiles():
-    return "select your profile"
-
-
-@app.route("/profiles/create", methods=["GET", "PUT"])
-def save_profile():
-    data = request.data
-
-    return data
-
+from flask import Flask, request, render_template
+from routes.controller import app 
 
 if __name__ == "__main__":
     app.run()
