04d31d6eb56717221485508a5f07a59ed6452e6a
JackL <jacek.langer@outlook.de>
2023-09-28 19:33:06 +0200

    feature: update endpoints

--- app/routes/ships.py
+++ app/routes/ships.py
@@ -1,5 +1,8 @@
+import uuid
+
 from flask import Blueprint, redirect, render_template, request
 
+from app.dal.service import ShipService
 from app.routes.constants import GET, POST
 from app.routes.validation.login_validation import verify_is_logged_in
 
@@ -21,12 +24,19 @@ def create_ship(user_id):
 
 
 @ship_pages.route("/edit/<int:ship_id>/<string:user_id>", methods=[GET, POST])
-def edit_ship(ship_id, user_id):
+def edit_ship(ship_id: int, user_id: str):
     if not verify_is_logged_in():
         return redirect("/login")
-
+    ship_service = ShipService()
+    ship = ship_service.get_by_id(ship_id)
     if request.method == POST:
         pass
+
     return render_template(
-        "edit.html", content="components/ship_edit.html", user_id=user_id
+        "edit.html", content="components/ship_edit.html", user_id=user_id, ship=ship
     )
+
+
+@ship_pages.route("/delete/<int:ship_id>/<string:user_id>")
+def delete_ship(ship_id: int, user_id: str):
+    return redirect("/user")
