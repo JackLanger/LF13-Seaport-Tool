cdeabe0b87cceaafb690d57a5250265688c5a259
Jack Langer <Jacek.langer@outlook.de>
2023-09-28 09:13:32 +0200

    feature: info pages

---
+++ app/routes/info.py
@@ -0,0 +1,34 @@
+from flask import Blueprint, redirect, url_for, flash, render_template, request
+
+
+from app.dal.service import UserService
+
+info_pages = Blueprint(
+    "info", __name__, static_folder="../../static", template_folder="../../templates"
+)
+
+
+@info_pages.route("/contact", methods=["GET", "POST"])
+def contact_form():
+    message = ""
+    if request.method == "POST":
+        # send message
+        message = (
+            "Thank you for your message, we will get back to you as soon as possible."
+        )
+        flash(message)
+    return render_template("contact_us.html")
+
+
+@info_pages.route("/privacy-policy")
+def privacy_policy():
+    return render_template(
+        "info_page.html", page_content="components/privacy_policy.html"
+    )
+
+
+@info_pages.route("/terms-of-service")
+def terms_of_use():
+    return render_template(
+        "info_page.html", page_content="components/terms_of_service.html"
+    )
