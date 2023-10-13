272a47bd955abe720ec595aeb544b2f305d7af08
vwgl <98028538+vwgl@users.noreply.github.com>
2023-10-13 08:27:36 +0200

    Class QuestProcessor (#51)
    * Added enum for algorithm type and the questProcessor class
    
    * chore(questProcessor.py): remove unnecessary getter and update return statement
    
    * feat(Algorithm): strategy pattern, Quest Algorithms
    
    ---------
    
    Co-authored-by: Vivien Wegel <v.wegel@tu-braunschweig.de>
    Co-authored-by: Jack Langer <62244804+JackLanger@users.noreply.github.com>

--- algorithm/questProcessor.py
+++ algorithm/questProcessor.py
@@ -1,3 +1,18 @@
+from app.enums.algorithm_type_enum import AlgorithmType
+from typing import List
+from app.models.quest import Quest
+from app.models.ship import Ship
+
+
+class Algorithm:
+    def calculate(self):
+        raise NotImplementedError()
+
 class QuestProcessor:
-    def __init__(self):
-        pass
+    def __init__(self, algorithm_type: Algorithm, ships: List[Ship], quest: Quest):
+        self.__algorithm_type = algorithm_type
+        self.__ships = ships
+        self.__quest = quest
+
+    def process_quest(self):
+        self.__algorithm_type.calculate()
---
+++ app/enums/algorithm_type_enum.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class AlgorithmType(Enum):
+    TIME_CRITICAL = "time_critical"
+    CAPACITY_CRITICAL = "capacity_critical"
