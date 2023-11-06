2228f2977940aca05a91b2fce718ef68fd67c7ec
Jack Langer <Jacek.langer@outlook.de>
2023-11-06 11:50:09 +0100

    chore: fixed some tests

--- algorithm/capacityAlgorithm.py
+++ algorithm/capacityAlgorithm.py
@@ -1,3 +1,4 @@
+import math
 from typing import List, Dict
 
 from app.models.quest import QuestDTO, Resource
@@ -9,19 +10,47 @@ class AlgoResult:
     round_count: int
     rounds: [Dict[str, List[ShipDTO]]]
 
+    def __init__(self):
+        self.round_count = 0
+        self.rounds = []
+
 
 class CapacityAlgorithm(Algorithm):
     def __init__(self, ships: List[ShipDTO], quest: QuestDTO):
         super().__init__(ships, quest)
 
+    @property
     def calculate(self):
+        def calculate_total_capacity(ships: List[ShipDTO]) -> int:
+            total_capacity = 0
+            for ship in ships:
+                total_capacity += ship.capacity
+            return total_capacity
+
         result = AlgoResult()
 
-        if not self.quest.resources:
-            result.round_count = 0
-            return result
+        ships_total_capacity = calculate_total_capacity(self.ships)
 
-        result.round_count = 1
-        result.rounds = [{"Holz": [self.ships]}]
+        if not self.quest.resources or ships_total_capacity == 0:
+            return result
+        # move as much material as possible by utilizing all ships in the quest.
+        # Do until the total amount is exhausted or the total capacity exceeds the amount to ship.
+        # Repeat for each resource. 0..n rounds where all ships are used to move a single resource.
+        for resource in self.quest.resources:
+            while resource.amount > 0:
+                if resource.amount >= ships_total_capacity:
+                    result.rounds.append({resource.name: self.ships})
+                    resource.amount -= ships_total_capacity
+                else:
+                    break
+
+        for i in range(len(self.ships)):
+            round = {}
+            for resource in self.quest.resources:
+                pass
+
+            result.rounds.append(round)
+
+        result.round_count = len(result.rounds)
 
         return result
--- algorithm/test/capacity.py
+++ algorithm/test/capacity.py
@@ -6,7 +6,7 @@ from app.models.quest import Resource, QuestDTO
 from app.models.ship import ShipDTO
 
 
-def get_result(round_count, *rounds: Dict[str, List[List[ShipDTO]]]) -> AlgoResult:
+def get_result(round_count, *rounds: Dict[str, List[ShipDTO]]) -> AlgoResult:
     res = AlgoResult()
     res.round_count = round_count
     res.rounds = [r for r in rounds]
@@ -38,54 +38,56 @@ class CapacityAlgorithmTest(unittest.TestCase):
         quest = create_quest()
         algo = CapacityAlgorithm(ships, quest)
         expected = get_result(0)
-        self.assert_equals(expected, algo.calculate())
+        self.assert_equals(expected, algo.calculate)
 
     def test_one_resource_one_trip(self):
         ships = [ShipDTO(1, "name: str", 1, 1, 1)]
         quest = create_quest(Resource("Holz", 1))
         algo = CapacityAlgorithm(ships, quest)
-        expected = get_result(1, {"Holz": [ships]})
-        self.assert_equals(expected, algo.calculate())
+        expected = get_result(1, {"Holz": ships})
+        self.assert_equals(expected, algo.calculate)
 
     def test_one_resource_two_Trips(self):
         resources = [Resource("Holz", 2)]
         ships = [get_ship(1, 1)]
         quest = QuestDTO(1, "quest", resources)
         algo = CapacityAlgorithm(ships, quest)
-        expected = get_result(2, {"Holz": [ships, ships]})
-        self.assert_equals(expected, algo.calculate())
+        expected = get_result(2, {"Holz": ships}, {"Holz": ships})
+        self.assert_equals(expected, algo.calculate)
 
     def test_two_resource_two_Trips(self):
         resources = [Resource("Holz", 1), Resource("Stein", 1)]
         ships = [get_ship(1, 1)]
         quest = QuestDTO(1, "quest", resources)
         algo = CapacityAlgorithm(ships, quest)
-        expected = get_result(2, {"Holz": [ships, ships]}, {"Stein": [ships, ships]})
-        self.assert_equals(expected, algo.calculate())
+        expected = get_result(2, {"Holz": ships}, {"Stein": ships})
+        self.assert_equals(expected, algo.calculate)
 
     def test_two_resource_one_Trips(self):
         resources = [Resource("Holz", 1), Resource("Stein", 2)]
         ships = [get_ship(1, 1), get_ship(2, 2)]
         quest = QuestDTO(1, "quest", resources)
         algo = CapacityAlgorithm(ships, quest)
-        expected = get_result(2, {"Holz": [[ships[0]]]}, {"Stein": [[ships[1]]]})
-        self.assert_equals(expected, algo.calculate())
+        expected = get_result(2, {"Holz": [ships[0]], "Stein": [ships[1]]})
+        self.assert_equals(expected, algo.calculate)
 
     def test_two_resource_two_Trips_with_two_ships(self):
         resources = [Resource("Holz", 3), Resource("Stein", 2)]
         ships = [get_ship(1, 1), get_ship(2, 2)]
         quest = QuestDTO(1, "quest", resources)
         algo = CapacityAlgorithm(ships, quest)
-        expected = get_result(2, {"Holz": [ships]}, {"Stein": [[ships[1]]]})
-        self.assert_equals(expected, algo.calculate())
+        expected = get_result(
+            2, {"Holz": ships, "Stein": [ships[1]]}, {"Stein": [ships[1]]}
+        )
+        self.assert_equals(expected, algo.calculate)
 
     def test_two_resource_two_Trips_with_two_ships_non_optimal_condition(self):
         resources = [Resource("Holz", 30), Resource("Stein", 25)]
         ships = [get_ship(1, 10), get_ship(2, 20)]
         quest = QuestDTO(1, "quest", resources)
         algo = CapacityAlgorithm(ships, quest)
-        expected = get_result(2, {"Holz": [ships]}, {"Stein": [ships]})
-        self.assert_equals(expected, algo.calculate())
+        expected = get_result(2, {"Holz": ships}, {"Stein": ships})
+        self.assert_equals(expected, algo.calculate)
 
 
 if __name__ == "__main__":
