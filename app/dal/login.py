e4a8c27bc7cccd8589c6d2bc9faa942f422137b5
thomas duerpisch <thomas.duerpisch@gmail.com>
2023-11-17 01:14:55 +0100

    login hinzugefügt speichert id und passwort hashed passwort mit salz gibt user zurück oder false

---
+++ app/dal/login.py
@@ -0,0 +1,33 @@
+import pickle
+from app.models.user import User
+from dal import Dal
+import hashlib
+
+
+class localObject:
+    def __init__(self,id,password):
+        self.id = id
+        self.password = password
+
+class login:    
+    def makehash(password):
+        salt = hashlib.md5(password.encode())
+        saltyPassword = password+salt
+        return hashlib.md5(saltyPassword.encode())
+        
+    def save(self,id,password):
+        data = localObject(id,self.makehash(password))
+        with open(f"app/dal/login/{data.id}.pkl",'wb') as f:
+            pickle.dump(data,f,pickle.HIGHEST_PROTOCOL)
+            
+    def load(self,id,password):
+        with open(f"app/dal/login/{id}.pkl",'rb') as f:
+            user = pickle.load(f)
+            if user:
+                if user.id == id and user.password == self.makehash(password):
+                    return Dal.load(id)
+                else:
+                    return False
+            else:
+                return False
+        
